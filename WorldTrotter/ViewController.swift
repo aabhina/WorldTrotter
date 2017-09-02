3f95fd9ab2b85e0de43e4e55269912e35f42cfc0
Anshul Abhinav <anshul@Anshuls-MacBook-Pro.local>
2017-09-02 16:48:41 -0600

    Initial Commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Anshul Abhinav on 9/2/17.
+//  Copyright © 2017 Anshul Abhinav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
